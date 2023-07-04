7a58843dea600e7e0d093c5aebf2f2ea2fce9a8f
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-05 01:24:45 +0500

    nth

--- bool20.py
+++ bool20.py
@@ -1,4 +1,5 @@
 def main(a):
+    a<10000 and a%1==0 and (1000<=a<10000 and (a%10+a//1000+(a//100%10)+(a//10%10))%2==1) or (100<=a<1000 and (a%10+a//100+a//10%10)%2==1) or ((10<=a<100 and (a//10+a%10)%2==1)) or ((0<=a<10 and a%2==1))
     """
     Given integer is less of 10000 . Check if the sum of digits is odd .
     Args:
@@ -7,4 +8,5 @@ def main(a):
         bool
     """
     # Write your code here
-    return 
+    return a<10000 and a%1==0 and (1000<=a<10000 and (a%10+a//1000+(a//100%10)+(a//10%10))%2==1) or (100<=a<1000 and (a%10+a//100+a//10%10)%2==1) or ((10<=a<100 and (a//10+a%10)%2==1)) or ((0<=a<10 and a%2==1))
+print(main(7))
