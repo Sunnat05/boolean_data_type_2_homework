6e268b9e47ed2fafb6f88cb4d7795ff1087c117b
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-04 23:59:08 +0500

    nth

--- bool13.py
+++ bool13.py
@@ -1,4 +1,5 @@
 def main(b):
+    number=((b%3==0 and not b%5==0) or (not b%3==0 and b%5==0))
     """
     Check if the given number is divisible by only one of 3 or 5.
     Args:
@@ -7,4 +8,5 @@ def main(b):
         bool
     """
     # Write your code here
-    return 
+    return number
+print(main(36))
