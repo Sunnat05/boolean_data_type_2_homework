3678a6190a9e0d55c4abdc9cfb8c0c1a9880754e
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-04 23:38:45 +0500

    nth

--- bool12.py
+++ bool12.py
@@ -1,4 +1,5 @@
 def main(a):
+    b=(a%3==0 or a%5==0)
     """
     Check if a given number is divisible by either 3 or 5.
     Args:
@@ -7,4 +8,5 @@ def main(a):
         bool
     """
     # Write your code here
-    return 
+    return b
+print(main(33))
