c2d2c9ca1d46d2ac44e29424827a01c2608dda54
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-05 09:33:27 +0500

    nth

--- bool19.py
+++ bool19.py
@@ -1,5 +1,5 @@
 def main(a):
-    a%4==0 and a**2>10000
+    a%4==0 and (a%400==0 or not a%100==0) and a**2>10000
     """
     Check if a given year is a leap year and the square of the year is greater than 10000.
     Args:
