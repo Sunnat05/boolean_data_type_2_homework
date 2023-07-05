6e0bf0830429a3fcee3711a275e1a2171e619b6b
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-05 09:47:15 +0500

    nth

--- bool19.py
+++ bool19.py
@@ -8,5 +8,5 @@ def main(a):
         bool
     """
     # Write your code here
-    return a%4==0  and a**2>10000 and a%1==0
+    return a%4==0  and a**2>10000 and type(a)==type(1)
 print(main(400))
