b92b909e9a771fa5c5647014a81d99da9b3b426c
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-05 09:31:08 +0500

    nth

--- bool18.py
+++ bool18.py
@@ -1,4 +1,5 @@
 def main(a):
+    a%4==0 and (a%400==0 or not a%100==0)
     """
     Check if a given year is a leap year. Leap year is  is divisible by 4 and 400 but not by 100.
     Args:
@@ -7,5 +8,5 @@ def main(a):
         bool
     """
     # Write your code here
-    return a%4==0 and a%400==0 or not a%100==0
+    return a%4==0 and (a%400==0 or not a%100==0)
 print(main(1704))
